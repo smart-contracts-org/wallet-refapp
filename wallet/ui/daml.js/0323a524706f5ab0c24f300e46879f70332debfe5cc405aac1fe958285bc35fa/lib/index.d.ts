f64dfbe37b1298de3a31a7a4ca006b6beccbf608
maxhsu-da <max.hsu@digitalasset.com>
2022-04-10 16:43:50 -0700

    add .DS_store to get ignore.

--- wallet/ui/daml.js/0323a524706f5ab0c24f300e46879f70332debfe5cc405aac1fe958285bc35fa/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as Daml from './Daml';
-export { Daml } ;
-export declare const packageId = '0323a524706f5ab0c24f300e46879f70332debfe5cc405aac1fe958285bc35fa';
--- wallet/ui/daml.js/9de3ae0b7b4e753f7ce2a5dcaa8d49ab4b92c4d8a8fb9589b5f8c893fbea5bd1/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as DA from './DA';
-export { DA } ;
-export declare const packageId = '9de3ae0b7b4e753f7ce2a5dcaa8d49ab4b92c4d8a8fb9589b5f8c893fbea5bd1';
