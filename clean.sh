9aeef9dee1195a13c860cc587c8b2f3c6f52010e
Alex Putkov <97638642+alexputkov-da@users.noreply.github.com>
2022-04-21 20:04:31 -0400

    Update clean.sh
    Swapped the order of directories in the last line to allow daml.js folder to be deleted when node_modules folder does not exist

--- clean.sh
+++ clean.sh
@@ -2,4 +2,4 @@
 cd main/Account && rm -r .daml && rm account.dar
 cd ../Asset && rm -r .daml && rm asset.dar 
 cd ../User && rm -r .daml && rm user.dar 
-cd ../../ui && rm -r node_modules && rm -r daml.js
+cd ../../ui && rm -r daml.js && rm -r node_modules
