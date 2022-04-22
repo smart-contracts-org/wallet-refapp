3a786302f0c35d329bf46a89eccbf99e7d9f2f64
maxhsu-da <max.hsu@digitalasset.com>
2022-04-22 10:18:42 -0700

    add more scripts.

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 # Deletes all locally built files
-cd main/Account && rm -r .daml && rm account.dar
-cd ../Asset && rm -r .daml && rm asset.dar 
-cd ../User && rm -r .daml && rm user.dar 
-cd ../../ui && rm -r daml.js && rm -r node_modules
+cd main/Account && rm -f -r .daml && rm -f account.dar
+cd ../Asset && rm -f -r .daml && rm -f asset.dar 
+cd ../User && rm -f -r .daml && rm -f user.dar 
+cd ../../triggers && rm -f -r .daml && rm -f triggers.dar 
+cd ../ui && rm -f -r daml.js && rm -f -r node_modules && rm -rf build
